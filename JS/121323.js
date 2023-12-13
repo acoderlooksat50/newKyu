a4bfb30bd34ee52d237129eb3d3f633ccdd0b7bf
acoderlooksat50 <acoderlooksat50@gmail.com>
2023-12-13 09:17:01 -0500

    add 121323.js

---
+++ JS/121323.js
@@ -0,0 +1,10 @@
+//Remove first and last character from a given string.
+
+function removeChar(str){
+    let newStr =str.substring(1,str.length-1)
+    return newStr
+  }
+
+/* One line
+let removeChar = (str) => str.substring(1,str.length-1)
+*/
